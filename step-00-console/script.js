255bd978fba9712cf1ebc594f87a5311e79e21f8
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2022-05-06 20:36:58 +0500

    Added Outline
    Added Outline

---
+++ step-00-console/script.js
@@ -0,0 +1,71 @@
+console.log("Hello JavaScript !");
+// OUTPUT :
+// Hello JavaScript !
+
+var name1 = "Ahmed Ayaz";
+console.log("This is" + name1);
+console.log("This is", name1);
+// OUTPUT :
+// This isAhmed Ayaz
+// This is Ahmed Ayaz
+
+var age = 18;
+console.log("my age is " + age);
+console.log("my age is ", age);
+// OUTPUT :
+// my age is 18 //(All is White)
+// my age is  18 //(18 is Yellow)
+
+console.log("Ayaz\tAyaz\nAyaz\tAyaz\nAyaz\tAyaz");
+// OUTPUT :
+// Ayaz    Ayaz
+// Ayaz    Ayaz
+// Ayaz    Ayaz
+
+console.log("Hammad");
+// OUTPUT:
+// Hammad
+
+console.log("Hammad\nHammad");
+// OUTPUT:
+// Hammad
+// Hammad
+
+console.log("Name = Hammad\nAge = 28\nCity = Karachi\nCountry = Pakistan");
+// OUTPUT:
+// Name = Hammad
+// Age = 28
+// City = Karachi
+// Country = Pakistan
+
+console.log("Hammad Hammad");
+// OUTPUT:
+// Hammad Hammad
+
+console.log("Hammad\tHammad");
+// OUTPUT:
+// Hammad  Hammad
+
+console.log("Hammad/tHammad");
+// OUTPUT:
+// Hammad/tHammad
+
+console.log("My name is\t'Hammad'");
+// OUTPUT:
+// My name is      'Hammad'
+
+console.log('My name is\t "Hammad" ');
+// OUTPUT:
+// My name is       "Hammad"
+
+console.log('My name is\t "Hammad" \\ ');
+// OUTPUT:
+// My name is       "Hammad" \
+
+console.log("My name is\t'Hammad'");
+// OUTPUT:
+// My name is      'Hammad'
+
+console.log("Hammad" + "Hammad" + "Hammad"); // Concatenation means plus strings
+// OUTPUT:
+// HammadHammadHammad
---
+++ step-01-variables/script.js
@@ -0,0 +1,5 @@
+// https://www.w3schools.com/js/js_variables.asp
+// https://www.w3schools.com/js/js_strings.asp
+// https://www.w3schools.com/js/js_numbers.asp
+// https://www.w3schools.com/js/js_arrays.asp
+// https://www.w3schools.com/js/js_objects.asp
